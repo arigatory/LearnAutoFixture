22ffc4b81922fa899df2d9e0b4bca342d6ff22bd
Ivan MSI <arigatory@gmail.com>
2022-03-20 20:52:52 +0300

    done

---
+++ DemoCode.Tests/EmailAddressDemos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using AutoFixture;
+using System.Net.Mail;
+
+namespace DemoCode.Tests
+{
+    public class EmailAddressDemos
+    {
+        [Fact]
+        public void Email()
+        {
+            var fixture = new Fixture();
+            //string localPart = fixture.Create<EmailAddressLocalPart>().LocalPart;
+            //string domain = fixture.Create<DomainName>().Domain;
+            //string fullAddress = $"{localPart}@{domain}";
+
+            var sut = new EmailMessage(fixture.Create<MailAddress>().Address, fixture.Create<string>(), fixture.Create<bool>());
+        }
+    }
+}
---
+++ DemoCode.Tests/SequenceDemo.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class SequenceDemo
+    {
+        [Fact]
+        public void SequenceOfStrings()
+        {
+            var fixture = new Fixture();
+
+            IEnumerable<string> messages = fixture.CreateMany<string>();
+        }
+
+        [Fact]
+        public void ExplicitNumberOfItems()
+        {
+            var fixture = new Fixture();
+
+            IEnumerable<int> numbers = fixture.CreateMany<int>(600);
+        }
+
+        [Fact]
+        public void AddingToExistingList()
+        {
+            var fixture = new Fixture();
+            var sut = new DebugMessageBuffer();
+
+            fixture.AddManyTo(sut.Messages, 10);
+            
+
+            //act
+
+            sut.WriteMessages();
+
+            Assert.Equal(10, sut.MessagesWritten);
+
+
+        }
+    }
+}
